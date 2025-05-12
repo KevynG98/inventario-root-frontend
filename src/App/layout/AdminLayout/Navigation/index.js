bdce2eb2ae05a2df085578d321f0ca2bbdcf208a
Kevyn Giron <kgprogrammer98@gmail.com>
2025-05-11 22:48:59 -0600

    [ADD] Se añadio la UI de seguros

--- src/App/layout/AdminLayout/Navigation/index.js
+++ src/App/layout/AdminLayout/Navigation/index.js
@@ -50,7 +50,7 @@ class Navigation extends Component {
                 { title: 'Egreso de Pacientes', url: '/dashboard/construccion/', icon: <FiLogOut />, roles: [1] },
                 { title: 'Consulta externa', url: '/dashboard/admisiones/consulta-externa/', icon: <FiUserCheck />, roles: [1] },
                 { title: 'Caja', url: '/dashboard/admisiones/caja/', icon: <FiCreditCard />, roles: [1] },
-                { title: 'Seguros', url: '/dashboard/construccion/', icon: <FiShield />, roles: [1] },
+                { title: 'Seguros', url: '/dashboard/admisiones/seguros/', icon: <FiShield />, roles: [1] },
                 { title: 'Estado de habitaciones', url: '/dashboard/admisiones/estado-habitacion', icon: <FiGrid />, roles: [1] }
               ]
             },
